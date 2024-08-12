31e4bc7fce3ce7aa0345aa6b7c0eaf2efd82168a
axseinga <urbanowicz.uk@gmail.com>
2024-08-12 18:04:42 +0100

    solution: module 2 lesson 1 jsdom example task

--- examples/module2/lesson1/jsdom-example/renderer.test.ts
+++ examples/module2/lesson1/jsdom-example/renderer.test.ts
@@ -18,11 +18,32 @@ describe('User renderer', () => {
     expect(Array.from(container.querySelectorAll('li'))).toHaveLength(3);
   });
 
+  test('should render correct data for all users if admin is rendering the list', () => {
+    localStorage.setItem('userRole', 'admin');
+
+    const container = document.createElement('div');
+    renderItems(container, users);
+    const listItems = Array.from(container.querySelectorAll('li'));
+    expect(listItems[0].textContent).toContain('Name: John, Age: 30');
+    expect(listItems[1].textContent).toContain('(Admin) Name: Jane, Age: 25');
+    expect(listItems[2].textContent).toContain('Name: Jack, Age: 40');
+  });
+
   test('should render only regular users if non-admin is rendering the list', () => {
     localStorage.setItem('userRole', 'user');
 
     const container = document.createElement('div');
     renderItems(container, users);
     expect(Array.from(container.querySelectorAll('li'))).toHaveLength(2);
+  });
+
+  test('should render correct data for regular users if non-admin is rendering the list', () => {
+    localStorage.setItem('userRole', 'user');
+
+    const container = document.createElement('div');
+    renderItems(container, users);
+    const listItems = Array.from(container.querySelectorAll('li'));
+    expect(listItems[0].textContent).toContain('Name: John, Age: 30');
+    expect(listItems[1].textContent).toContain('Name: Jack, Age: 40');
   });
 });
